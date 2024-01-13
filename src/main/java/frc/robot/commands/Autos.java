ccfdf5ae9edad525ac1c832073cb27e54a14373e
Johnathon Reidel <jreidel8@gmail.com>
2024-01-12 18:22:25 -0800

    drive

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -24,8 +24,11 @@ public class Autos extends Command{
   SwerveDrive swerveDrive;
   SwerveRequest.ApplyChassisSpeeds autonDrive = new SwerveRequest.ApplyChassisSpeeds();
   ShooterSubsystem shooter = new ShooterSubsystem();
-  HolonomicPathFollowerConfig pathFollowerConfig = new HolonomicPathFollowerConfig(new PIDConstants(.2, .0001,0), new PIDConstants(.1, .1,0), 5, .33, new ReplanningConfig());
-  public SendableChooser<Command> autonChooser = new SendableChooser<>();
+  HolonomicPathFollowerConfig pathFollowerConfig = new HolonomicPathFollowerConfig(new PIDConstants(.4, .001,0), new PIDConstants(.2, .01,0), 5, .33, new ReplanningConfig());
+  public SendableChooser<Command> autonChooserRed = new SendableChooser<>();
+  public SendableChooser<Command> autonChooserBlue = new SendableChooser<>();
+  public SendableChooser<Boolean> side = new SendableChooser<>();
+
   private HashMap<String, Command> eventMap;
 
 
@@ -34,20 +37,26 @@ public class Autos extends Command{
         AutoBuilder.configureHolonomic(()-> swerveDrive.getPose(), swerveDrive::seedFieldRelative, swerveDrive::getCurrentRobotChassisSpeeds, (speeds)-> swerveDrive.setControl(autonDrive.withSpeeds(speeds)), pathFollowerConfig, ()-> false, swerveDrive);
     eventMap = new HashMap<>();
 
-    Shuffleboard.getTab("Auton").add("Auton Style",autonChooser)
-            .withWidget(BuiltInWidgets.kComboBoxChooser)
-            .withPosition(0, 0)
-            .withSize(2, 1);;
+    Shuffleboard.getTab("Autons").add("Side", side);
+    side.addOption("Red Side", true);
+    side.addOption("Blue Side", false);
+
+    Shuffleboard.getTab("Autons").add("Auton Style Red", autonChooserRed).withWidget(BuiltInWidgets.kComboBoxChooser).withPosition(0, 0).withSize(2, 1);
+    autonChooserRed.addOption("Do nothing", doNothing());
+    autonChooserRed.addOption("Shoot and Taxi Middle", shootTaxiMiddleRed());
 
 
-    autonChooser.addOption("Do nothing", doNothing());
+    Shuffleboard.getTab("Autons").add("Auton Style Blue", autonChooserBlue).withWidget(BuiltInWidgets.kComboBoxChooser).withPosition(0, 0).withSize(2, 1);
+
+
+
 
   }
   public Command getAuton(){
-    return autonChooser.getSelected();
+    return autonChooserRed.getSelected();
   }
   
-
+  //Autons that don't move
   public Command doNothing(){
         return new WaitCommand(15);
 
@@ -55,13 +64,58 @@ public class Autos extends Command{
   public Command shootDontMove(){
     return new WaitCommand(15);
   }
-  public Command shootTaxiRed(){
-    return new PathPlannerAuto("Shoot and Taxi Middle");
+
+  //Taxi Autons
+  public Command taxiTopRed(){
+    return new PathPlannerAuto("Taxi Top Red");
+  }
+    public Command taxiMiddleRed(){
+    return new PathPlannerAuto("Taxi Middle Red");
+  }
+    public Command taxiBottomRed(){
+    return new PathPlannerAuto("Taxi Bottom Red");
+  }
+    public Command taxiTopBlue(){
+    return new PathPlannerAuto("Taxi Top Blue");
+  }
+    public Command taxiMiddleBlue(){
+    return new PathPlannerAuto("Taxi Middle Blue");
+  }
+    public Command taxiBottomBlue(){
+    return new PathPlannerAuto("Taxi Bottom Blue");
+  }
+
+  //One piece Autons
+  public Command shootTaxiTopRed(){
+    return new PathPlannerAuto("Shoot and Taxi Top Red");
+  }
+  public Command shootTaxiMiddleRed(){
+    return new PathPlannerAuto("Shoot and Taxi Middle Red");
+  }
+  public Command shootTaxiBottomRed(){
+    return new PathPlannerAuto("Shoot and Taxi Bottom Red");
+  }
+
+  public Command shootTaxiTopBlue(){
+    return new PathPlannerAuto("Shoot and Taxi Top Blue");
+  }
+    public Command shootTaxiMiddleBlue(){
+    return new PathPlannerAuto("Shoot and Taxi Middle Blue");
+  }
+    public Command shootTaxiBottomBlue(){
+    return new PathPlannerAuto("Shoot and Taxi Bottom Blue");
+  }
+
+
+  //Two Piece autons
+  public Command twoPieceTopRed(){
+    return new PathPlannerAuto("2 Piece Top Red");
   }
   public Command twoPieceExtendedRed(){
     return new PathPlannerAuto("2 Piece Center Top Red");
     }
 
+  //Three Piece Autons
   public Command threePieceTtMBlue(){
     return new PathPlannerAuto("3 Piece Top Blue");
   }
@@ -74,9 +128,6 @@ public class Autos extends Command{
     return new PathPlannerAuto("4 Piece Blue");
   }
 
-  public Command test(){
-    return new PathPlannerAuto("New Auto");
-  }
 
 
 
