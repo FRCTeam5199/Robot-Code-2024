01793191ec4b3583f86e21337bc73a56253b1d44
Unsung444 <107723083+Unsung444@users.noreply.github.com>
2024-01-17 19:18:13 -0800

    s

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 
-import com.frc.robot.Constants;
-import com.frc.robot.abstractMotorInterfaces.TalonMotorController;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import frc.robot.Main;
+import frc.robot.constants.MainConstants;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -10,48 +12,35 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
   
-  public TalonMotorController rotateMotorController;
+	public MainConstants constants = new MainConstants();
+
 	PIDController rotatePIDController;
 	
-
   public Arm() {
+	TalonFX krakenArmFollower;
+	krakenArmFollower = new TalonFX(constants.krakenArmFollower);
+
+	PIDInit();
   }
 
   public void init(){
-    motorinit();
-    PIDinit();
 
-		rotateMotorController.resetEncoder();
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-    rotateMotorController.moveAtPercent(rotatePIDController.calculate(rotateMotorController.getRotations()));
+    
   }
 
-	public void motorInit() {
-		rotateMotorController = new TalonMotorController(Constants.ARM_ROTATE_MOTOR_ID, MotorType.kBrushless);
-
-		rotateMotorController.setBrake(true);
-	}
-
   public void PIDInit() {
-		rotatePIDController = new PIDController(Constants.ARM_ROTATE_PID.P, Constants.ARM_ROTATE_PID.I,
-				Constants.ARM_ROTATE_PID.D);
+		rotatePIDController = new PIDController(MainConstants.PIDConstants.ARM_FOLLOWER_PID.P, MainConstants.PIDConstants.ARM_FOLLOWER_PID.I,
+				MainConstants.PIDConstants.ARM_FOLLOWER_PID.D);
 	}
 
-	public Command resetRotateEncoder() {
-		return this.runOnce(() -> rotateMotorController.resetEncoder());
-	}
 
 	public Command setRotateSetpoint(int setpoint) {
 		return this.runOnce(() -> rotatePIDController.setSetpoint(setpoint));
 	}
 
-  public Command moveRotate(float percent) {
-		return this.runEnd(() -> rotateMotorController.moveAtPercent(percent),
-				() -> rotateMotorController.moveAtPercent(0));
-  }
   
 }
