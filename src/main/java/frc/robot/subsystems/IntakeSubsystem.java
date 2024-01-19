7e8bf3942024c61365a59d22ad491cc0d5f8744b
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2024-01-18 18:56:57 -0800

    called methods

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -12,52 +12,53 @@ import frc.robot.abstractMotorInterfaces.VortexMotorController;
 import frc.robot.constants.MainConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
-  public VortexMotorController intakeMotor;
-  public VortexMotorController intakeAngleMotor;
-  public PIDController pidController;
-  public double setpoint;
+    public VortexMotorController intakeMotor;
+    public VortexMotorController intakeAngleMotor;
+    public PIDController pidController;
+    public double setpoint;
 
-  public IntakeSubsystem() {
-    init();
-  } 
+    public IntakeSubsystem() {
+        init();
+    }
 
-  public void init() {
-    motorInit();
-  }
+    public void init() {
+        motorInit();
+        PIDInit();
+    }
 
-  @Override
-  public void periodic() {
-    intakeMotor.set(pidController.calculate(intakeAngleMotor.getRotations(), setpoint));
-  }
+    @Override
+    public void periodic() {
+        intakeAngleMotor.set(pidController.calculate(intakeAngleMotor.getRotations(), setpoint));
+    }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
 
-  public void motorInit() {
-    intakeMotor = new VortexMotorController(MainConstants.IDs.Motors.INTAKE_MOTOR_ID);
-    intakeAngleMotor = new VortexMotorController(MainConstants.IDs.Motors.INTAKE_ANGLE_MOTOR_ID);
+    public void motorInit() {
+        intakeMotor = new VortexMotorController(MainConstants.IDs.Motors.INTAKE_MOTOR_ID);
+        intakeAngleMotor = new VortexMotorController(MainConstants.IDs.Motors.INTAKE_ANGLE_MOTOR_ID);
 
-    intakeMotor.setPosition(0);
-    intakeAngleMotor.setPosition(0);
-  }
+        intakeMotor.setPosition(0);
+        intakeAngleMotor.setPosition(0);
+    }
 
-  public void PIDInit() {
-    pidController.setP(MainConstants.PIDConstants.INTAKE_PID.P);
-    pidController.setP(MainConstants.PIDConstants.INTAKE_PID.I);
-    pidController.setP(MainConstants.PIDConstants.INTAKE_PID.D);
-  }
+    public void PIDInit() {
+        pidController.setP(MainConstants.PIDConstants.INTAKE_PID.P);
+        pidController.setP(MainConstants.PIDConstants.INTAKE_PID.I);
+        pidController.setP(MainConstants.PIDConstants.INTAKE_PID.D);
+    }
 
-  public Command setIntakeSpeed(double percent) {
-    return this.runOnce(() -> intakeMotor.set(percent));
-  }
+    public Command setIntakeSpeed(double percent) {
+        return this.runOnce(() -> intakeMotor.set(percent));
+    }
 
-  public Command stowIntake() {
-    return this.runOnce(() -> setpoint = MainConstants.Setpoints.STOW_INTAKE);
-  }
+    public Command stowIntake() {
+        return this.runOnce(() -> setpoint = MainConstants.Setpoints.STOW_INTAKE);
+    }
 
-  public Command deployIntake() {
-    return this.runOnce(() -> setpoint = MainConstants.Setpoints.DEPLOY_INTAKE);
-  }
+    public Command deployIntake() {
+        return this.runOnce(() -> setpoint = MainConstants.Setpoints.DEPLOY_INTAKE);
+    }
 }
