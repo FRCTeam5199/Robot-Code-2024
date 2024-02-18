8f496a5b8dc74394897c24e4a9e95ea38219289f
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2024-02-18 13:52:50 -0800

    changed arm setpoints for control panel

--- src/main/java/frc/robot/constants/MainConstants.java
+++ src/main/java/frc/robot/constants/MainConstants.java
@@ -170,12 +170,12 @@ public class MainConstants {
         public static final double ARM_BOTTOM_PIECE_SETPOINT = 73;
         public static final double ARM_MIDDLE_PIECE_SETPOINT = 73;
         //Control Panel
-        public static final double ARM_SUBWOOFER_SETPOINT = 75.75;
+        public static final double ARM_SUBWOOFER_SETPOINT = 82;
         public static final double ARM_SPEAKER_BACK_SETPOINT = 160;
         public static final double ARM_SAFE_SETPOINT = 62.21;
         public static final double ARM_AMP_SETPOINT = 74.7;
         public static final double ARM_FAR_SHOT_SETPOINT = 65;
-        public static final double ARM_HP_STATION_SETPOINT = 80;
+        public static final double ARM_HP_STATION_SETPOINT = 150;
 
     }
 }
