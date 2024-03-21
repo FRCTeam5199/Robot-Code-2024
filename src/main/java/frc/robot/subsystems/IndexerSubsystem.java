44f28de9c14a0d81188167a61c74c3e1d6d72e82
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2024-03-20 21:05:04 -0700

    added servo

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -4,41 +4,53 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Main;
 import frc.robot.abstractMotorInterfaces.VortexMotorController;
 import frc.robot.constants.MainConstants;
 
 public class IndexerSubsystem extends SubsystemBase {
-  /** Creates a new indexer. */
-  public VortexMotorController shooterIndexerMotor;
+    /**
+     * Creates a new indexer.
+     */
+    public VortexMotorController shooterIndexerMotor;
+    public Servo servo;
 
-  public IndexerSubsystem() {
+    public IndexerSubsystem() {
 
-  }
+    }
 
-  public void init(){
-    shooterIndexerMotor = new VortexMotorController(MainConstants.IDs.Motors.SHOOTER_INDEXER_MOTOR_ID);
+    public void init() {
+        shooterIndexerMotor = new VortexMotorController(MainConstants.IDs.Motors.SHOOTER_INDEXER_MOTOR_ID);
 
-      
-      shooterIndexerMotor.setInvert(false);
-      shooterIndexerMotor.setBrake(true);
+        shooterIndexerMotor.setInvert(false);
+        shooterIndexerMotor.setBrake(true);
 
-      shooterIndexerMotor.setCurrentLimit(40);
-  }
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+        shooterIndexerMotor.setCurrentLimit(40);
 
-  public Command intakeIndexerForShooting(double speed, double s) {
+        servo = new Servo(MainConstants.IDs.SERVO_ID);
+    }
+
+    public void moveServo(double angle) {
+        servo.setAngle(angle);
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    public Command intakeIndexerForShooting(double speed, double s) {
         return this.runOnce(() -> shooterIndexerMotor.set(speed)).andThen(new WaitCommand(s)).andThen(setIndexerSpeed(0));
-      }
-      public Command setIndexerSpeed(double percent) {
+    }
+
+    public Command setIndexerSpeed(double percent) {
         return this.runOnce(() -> shooterIndexerMotor.set(percent));
     }
-        /**
+    /**
      * Stops the Shooter Motor
      */
 
@@ -48,18 +60,19 @@ public class IndexerSubsystem extends SubsystemBase {
      * @return True if a game piece is in the Indexer
      */
     private boolean currentCheck() {
-      new WaitCommand(0.4);
-      if (shooterIndexerMotor.getCurrent() < 39.8) {
-          return false;
-      }
-      if (shooterIndexerMotor.getCurrent() > 39.8) {
-          return true;
-      }
-      return false;
-  }
-  public boolean checkForGamePiece() {
-    int piece = 0;
-    int noPiece = 0;
+        new WaitCommand(0.4);
+        if (shooterIndexerMotor.getCurrent() < 39.8) {
+            return false;
+        }
+        if (shooterIndexerMotor.getCurrent() > 39.8) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkForGamePiece() {
+        int piece = 0;
+        int noPiece = 0;
         if (shooterIndexerMotor.getCurrent() > 39.8) {
             for (int i = 0; i <= 10; i++) {
                 if (currentCheck() == true) {
@@ -67,10 +80,10 @@ public class IndexerSubsystem extends SubsystemBase {
                 }
             }
         }
-    new WaitCommand(0.5);
-    if (piece > noPiece) {
-        return true;
+        new WaitCommand(0.5);
+        if (piece > noPiece) {
+            return true;
+        }
+        return false;
     }
-    return false;
-}
 }
